044a80ba633d718999fca9b17b8df83d3b76d8d6
MrJerry <my_main.com>
2025-06-04 14:39:23 +0300

    9 Added scanner menu in class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,11 @@ import service.DepositService;
 import service.PaymentService;
 import service.TransferService;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        // Initial
         Person amy = new Person("Amy", "Wong", +123123123);
         Bill amyBill = new Bill(1200);
         Account amyAccount = new Account(amy, amyBill);
@@ -15,6 +17,52 @@ public class Main {
         Person lila = new Person("Lila", "Tauranga", +123123123);
         Bill lilaBill = new Bill(700);
         Account lilaAccount = new Account(lila, lilaBill);
+
+        PaymentService paymentService = new PaymentService();
+        DepositService depositService = new DepositService();
+        TransferService transferService = new TransferService();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("\nВыберите действие:");
+            System.out.println("1 - Пополнение");
+            System.out.println("2 - Оплата");
+            System.out.println("3 - Перевод");
+            System.out.println("0 - Выход");
+
+            String userInput = scanner.nextLine();
+            switch (userInput){
+                case "1" ->{
+                    System.out.print("Сколько хотите пополнить? ");
+                    int amount = Integer.parseInt(scanner.nextLine());
+                    depositService.deposit(amyAccount, amount);
+                }
+                case "2" -> {
+                    System.out.print("Сколько хотите оплатить? ");
+                    int amount = Integer.parseInt(scanner.nextLine());
+                    paymentService.pay(amyAccount, amount);
+                }
+                case "3" -> {
+                    System.out.print("Сколько хотите перевести? ");
+                    int amount = Integer.parseInt(scanner.nextLine());
+                    transferService.transferCash(amyAccount, lilaAccount, amount);
+                }
+                case "0" -> {
+                    System.out.println("Пока!");
+                    return;
+                }
+                default -> System.out.println("Неверный ввод");
+            }
+        }
+
+
+//        Person amy = new Person("Amy", "Wong", +123123123);
+//        Bill amyBill = new Bill(1200);
+//        Account amyAccount = new Account(amy, amyBill);
+//
+//        Person lila = new Person("Lila", "Tauranga", +123123123);
+//        Bill lilaBill = new Bill(700);
+//        Account lilaAccount = new Account(lila, lilaBill);
 
 //        PaymentService paymentService = new PaymentService();
 //        paymentService.pay(amyAccount, 1000);
@@ -24,8 +72,8 @@ public class Main {
 //        depositService.deposit(amyAccount, -10);
 //        depositService.deposit(lilaAccount, 1111);
 
-        TransferService transferService = new TransferService();
-        transferService.transferCash(amyAccount, lilaAccount, 1000);
+//        TransferService transferService = new TransferService();
+//        transferService.transferCash(amyAccount, lilaAccount, 1000);
 
     }
 }
